76f6dd972af829cf7ca42a4d65a303b057fd2e0f
geody2000 <117552031+geody2000@users.noreply.github.com>
2023-03-09 03:44:55 +0200

    update

---
+++ tom-grooves/groovybox/random-video-loader.js
@@ -0,0 +1,18 @@
+for (var i = 0; i < videoUrls.length; i++) {
+    var url = videoUrls[i].url;
+    var embedUrl = url.replace('watch?v=', 'embed/');
+    videoUrls[i].url = embedUrl;
+  }
+  
+  function changeVideo() {
+    var randomIndex = Math.floor(Math.random() * videoUrls.length);
+    var randomUrl = videoUrls[randomIndex].url;
+    var videoPlayer = document.getElementById("video-player");
+    videoPlayer.src = randomUrl;
+  }
+  
+  document.addEventListener("DOMContentLoaded", () => {
+    changeVideo();
+    document.getElementById("change-video").addEventListener("click", changeVideo);
+  });
+  
---
+++ tom-grooves/groovybox/shuffle.js
@@ -0,0 +1,95 @@
+var videoUrls = [
+  {
+    url: 'https://www.youtube.com/watch?v=SAa4teWb0rU',
+    title: 'Ella Fitzgerald - Blue Skies'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=EahKmJ4-tjY',
+    title: 'Ella Fitzgerald - The Lady Is A Tramp'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=r1C69fzfGBM',
+    title: 'Doris Day - Que Sera, Sera (Whatever Will Be, Will Be)'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=wmRHeKF2kdU',
+    title: 'Dean Martin - Everybody Loves Somebody'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=-UbpcAqRowU',
+    title: 'Perry Como - Killing Me Softly With Her Song'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=TR1Y1qxkbLE',
+    title: 'The Carpenters - Superstar'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=V6MvuFGEapo',
+    title: 'John Denver - Leaving On A Jet Plane'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=c0dvabcWVo0',
+    title: 'John Denver - Take Me Home, Country Roads (Original Version)'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=YkLLcIKhJ64',
+    title: 'ABBA - Dancing Queen'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=KMViJKmAV4M',
+    title: 'ABBA - Mamma Mia'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=WbnG3eAGb6Y',
+    title: 'ABBA - The Winner Takes It All'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=GKdl-GCsNJ0',
+    title: 'The Beatles - Here Comes The Sun (2019 Mix)'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=BdEe5SpdIuo',
+    title: 'Elton John - Rocket Man (I Think It\'s Going To Be A Long, Long Time)'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=H6M0EulApMM',
+    title: 'Billy Joel - Uptown Girl'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=3cKtSlsYVEU',
+    title: 'Earth, Wind & Fire - September'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=bJ8Sz8CJY5g',
+    title: 'ELO - Mr. Blue Sky'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=xNDRrpG1Gc4',
+    title: 'Gloria Gaynor - I Will Survive'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=kvazBqAlx58',
+    title: 'Paul Anka - Put Your Head On My Shoulder'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=DYwQy_9JPtQ',
+    title: 'Frankie Valli And The Four Seasons - Can\'t Take My Eyes Off You'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=le1QF3uoQNg',
+    title: 'Frank Sinatra - Theme From New York, New York (2008 Remastered)'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=kbRindAErlo',
+    title: 'Dean Martin - That\'s Amore'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=8WyfVoFM9As',
+    title: 'ABBA -  Chiquitita'
+  }
+];
+
+
+
+
+
