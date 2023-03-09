f79f73cb28f07963491ced876e19f6b6dc477261
geody2000 <117552031+geody2000@users.noreply.github.com>
2023-03-09 03:59:26 +0200

    test

--- tom-grooves/groovybox/random-video-loader.js
+++ tom-grooves/groovybox/random-video-loader.js
@@ -1,19 +1,21 @@
+// Modify URLs to use embed links
 for (var i = 0; i < videoUrls.length; i++) {
-    var url = videoUrls[i].url;
-    var embedUrl = url.replace('watch?v=', 'embed/');
-    videoUrls[i].url = embedUrl;
-  }
-  
-  function changeVideo() {
-    var randomIndex = Math.floor(Math.random() * videoUrls.length);
-    var randomUrl = videoUrls[randomIndex].url;
-    var videoPlayer = document.getElementById("video-player");
-    videoPlayer.src = randomUrl;
-    videoPlayer.addEventListener('ended', changeVideo);
-  }
-  
-  document.addEventListener("DOMContentLoaded", () => {
-    changeVideo();
-    document.getElementById("change-video").addEventListener("click", changeVideo);
-  });
-  
+  var url = videoUrls[i].url;
+  var embedUrl = url.replace('watch?v=', 'embed/');
+  videoUrls[i].url = embedUrl;
+}
+
+// Change the video
+function changeVideo() {
+  var randomIndex = Math.floor(Math.random() * videoUrls.length);
+  var randomUrl = videoUrls[randomIndex].url;
+  var videoPlayer = document.getElementById("video-player");
+  videoPlayer.src = randomUrl;
+}
+
+// Call changeVideo when the page loads, and when the video ends
+document.addEventListener("DOMContentLoaded", () => {
+  changeVideo();
+  var videoPlayer = document.getElementById("video-player");
+  videoPlayer.addEventListener('ended', changeVideo);
+});
